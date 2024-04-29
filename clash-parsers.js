245c13d999b1e889501f60f6d5af98e5a21f5668
AgainZhao <safetyMail7@protonmail.com>
2024-04-29 13:26:08 +0800

    update parsers

--- clash-parsers.js
+++ clash-parsers.js
@@ -1,3 +1,6 @@
 module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url, interval, selected }) => {
-  return `# parsed by remote parser\n\n${raw}`
+  const rawObj = yaml.parse(raw)
+  const groups = []
+  const rules = []
+  return yaml.stringify({ ...rawObj, 'proxy-groups': groups, rules })
 }
